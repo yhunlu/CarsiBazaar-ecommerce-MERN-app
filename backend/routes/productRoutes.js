b63456476da623fa90f7251ffd77639ea2534bec
yhunlu <yhunlu87@gmail.com>
2021-10-16 01:40:47 +0300

    added errorMiddleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -23,7 +23,10 @@ router.get(
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    if (!product) return res.status(404).json({ message: "Product not found" });
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
 
     res.json(product);
   })
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,7 +5,7 @@ import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className="my-4 p-4 rounded">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
