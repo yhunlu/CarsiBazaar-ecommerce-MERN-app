bae571e7ed0db345fa56ff8c1316ec395e3ce4b3
yhunlu <yhunlu87@gmail.com>
2021-10-15 22:57:23 +0300

    added productRoutes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
+
+// @desc Fetch all products
+// @routes GET /api/products
+// @access Public
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @desc Fetch single products
+// @routes GET /api/products/:id
+// @access Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    res.json(product);
+  })
+);
+
+export default router;
