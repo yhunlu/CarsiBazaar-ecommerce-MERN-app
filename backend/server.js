d92d0abed24c3801cf94c12ec6a5113596cfbb60
yhunlu <yhunlu87@gmail.com>
2021-10-23 17:58:43 +0300

    added userRoutes and controller as well as implemented Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -11,11 +12,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
