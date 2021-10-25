5afb1545e72b11619d2826b083c0849ebe874cc5
yhunlu <yhunlu87@gmail.com>
2021-10-26 01:00:02 +0300

    added registerScreen and implemented reducer and action

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { register } from "../store/users";
+import FormContainer from "../components/FormContainer";
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.entities.users);
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // DISPATCH LOGIN
+    if (password !== confirmPassword) {
+      setMessage("Şifreler Aynı Değil !");
+    } else {
+      dispatch(register(name, email, password));
+    }
+  };
+
+  return (
+    <FormContainer className="login">
+      <h1>Üye Ol</h1>
+      {message && <Message variant="danger">{message}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="name">
+          <Form.Label>İsim</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="İsmini Yaz"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email Adresini Yaz"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Şifre</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Şifreni Yaz"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Tekrar Şifre</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Tekrar Şifreni Yaz"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Üye Ol
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          Zaten Üye misin ?{" "}
+          <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+            Giriş Yap
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -25,6 +25,19 @@ const slice = createSlice({
     usersLogout: (users, action) => {
       users.userInfo = null;
     },
+    registerRequested: (users, action) => {
+      users.loading = true;
+    },
+    registerRequestFailed: (users, action) => {
+      users.loading = false;
+      users.error = action.payload;
+    },
+    // register/registerReceived
+    registerReceived: (users, action) => {
+      users.userInfo = action.payload;
+      users.loading = false;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
   },
 });
 
@@ -32,11 +45,16 @@ export const {
   usersReceived,
   usersRequested,
   usersRequestFailed,
+  registerReceived,
+  registerRequested,
+  registerRequestFailed,
   usersLogout,
 } = slice.actions;
 export default slice.reducer;
 
 // Action Creators
+
+// Login
 const url = "/users/login";
 // () => fn (dispatch, getState)
 export const login = (email, password) => async (dispatch, getState) => {
@@ -63,3 +81,27 @@ export const logout = () => (dispatch, getState) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: usersLogout.type });
 };
+
+// Register
+const url_register = "/users";
+// () => fn (dispatch, getState)
+export const register =
+  (name, email, password) => async (dispatch, getState) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    dispatch(
+      apiCallBegan({
+        url: url_register,
+        data: { name, email, password },
+        config,
+        method: "POST",
+        onStart: registerRequested.type,
+        onSuccess: registerReceived.type,
+        onError: registerRequestFailed.type,
+      })
+    );
+  };
