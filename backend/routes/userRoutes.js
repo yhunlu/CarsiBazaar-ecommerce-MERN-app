c725697e40d67d3084dece29c1f8d4807f7f720c
mrsbluegreen <mrsbluegreen@hotmail.com>
2021-10-28 01:57:12 +0300

    implemented profile endpoint regarding update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -10,6 +11,6 @@ const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
