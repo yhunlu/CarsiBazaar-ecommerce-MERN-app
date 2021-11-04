1c9f236547f8c8a7c36921cefbcde49f2fb23d41
mrsbluegreen <mrsbluegreen@hotmail.com>
2021-11-05 02:18:34 +0300

    implemented getOrderID endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -23,4 +23,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
     }
 });
 
-export { addOrderItems }
+// @desc Get order by ID
+// @routes GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if (order) {
+        res.json(order);
+    } else {
+        res.status(404)
+        throw new Error("Order not found")
+    }
+});
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-    addOrderItems
+    addOrderItems, getOrderById
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router;
