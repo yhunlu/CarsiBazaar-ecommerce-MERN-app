1c5325952098d1c2e67d6a5b6850f5e5f45798bb
yhunlu <yhunlu87@gmail.com>
2022-08-03 02:33:55 +0300

    Prepare for production.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import colors from "colors";
-import path from "path";
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import colors from 'colors';
+import path from 'path';
 
-import cors from "cors";
-import Stripe from "stripe";
-import { v4 as uuid } from "uuid";
+import cors from 'cors';
+import Stripe from 'stripe';
+import { v4 as uuid } from 'uuid';
 
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 // import { createCheckoutSession } from "./api/checkout.js";
 
 dotenv.config();
@@ -22,8 +22,8 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 const stripe = new Stripe(process.env.STRIPE_CLIENT_SECRET_KEY);
@@ -31,19 +31,15 @@ const stripe = new Stripe(process.env.STRIPE_CLIENT_SECRET_KEY);
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 // app.post("/create-checkout-session", createCheckoutSession);
 
-app.post("/api/config/stripe", async (req, res) => {
-  console.log("Request:", req.body);
+app.post('/api/config/stripe', async (req, res) => {
+  console.log('Request:', req.body);
 
   let error;
   let status;
@@ -59,7 +55,7 @@ app.post("/api/config/stripe", async (req, res) => {
     const charge = await stripe.charges.create(
       {
         amount: order.totalPrice * 100,
-        currency: "TRY",
+        currency: 'TRY',
         customer: customer.id,
         receipt_email: token.email,
       },
@@ -67,18 +63,31 @@ app.post("/api/config/stripe", async (req, res) => {
         idempotencyKey,
       }
     );
-    console.log("Charge:", { charge });
-    status = "success";
+    console.log('Charge:', { charge });
+    status = 'success';
   } catch (error) {
-    console.error("Error:", error);
-    status = "failure";
+    console.error('Error:', error);
+    status = 'failure';
   }
 
   res.json({ error, status });
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  // go to find static files from frontend
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
